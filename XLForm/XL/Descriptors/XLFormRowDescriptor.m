2f2badd2654729a89e685879c9fbc5c6e50fde2c
Ben Chan <ben.chan@sinodynamic.com>
2016-06-29 16:08:23 +0800

    -Add whitespace checking for value

--- XLForm/XL/Descriptors/XLFormRowDescriptor.m
+++ XLForm/XL/Descriptors/XLFormRowDescriptor.m
@@ -485,8 +485,11 @@ CGFloat XLFormRowInitialHeight = -2;
 
 - (BOOL)valueIsEmpty
 {
+    NSCharacterSet *set = [NSCharacterSet whitespaceCharacterSet];
+    
     return self.value == nil || [self.value isKindOfClass:[NSNull class]] || ([self.value respondsToSelector:@selector(length)] && [self.value length]==0) ||
-    ([self.value respondsToSelector:@selector(count)] && [self.value count]==0);
+    ([self.value respondsToSelector:@selector(count)] && [self.value count]==0) ||
+    ([self.value respondsToSelector:@selector(stringByTrimmingCharactersInSet:)] && [[self.value stringByTrimmingCharactersInSet: set] length] == 0);
 }
 
 -(XLFormValidationStatus *)doValidation
